27c8a9ace72b67bedc426d90e997d8e42a6ffe01
Kareem20238 <kareemzaian7@gmail.com>
2025-11-20 03:15:04 +0200

    second

--- collision_shape_2d.gd
+++ collision_shape_2d.gd
@@ -1,11 +1,18 @@
-extends CollisionShape2D
+extends CharacterBody2D
 
+var speed = 200
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta):
+	var direction = Vector2.ZERO
 
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	velocity = direction.normalized() * speed
+	move_and_slide()
